d31459e7d38f14727743275dfc47d645ac2118f5
IdlanAsyraaf <116176668+IdlanAsyraaf@users.noreply.github.com>
2023-06-12 22:33:33 +0800

    Add files via upload

---
+++ TicTacToe/TicTacToeNormal.java
@@ -0,0 +1,158 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tictactoe;
+
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Idlan
+ */
+public class TicTacToeNormal extends JFrame {
+    private Container pane;
+    private String currentPlayer;
+    private JButton [][] board;
+    private boolean hasWinner;
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem quit;
+    private JMenuItem newGame;
+    
+    public TicTacToeNormal(){
+        super();
+        pane = getContentPane();
+        pane.setLayout(new GridLayout(5,5));
+        setTitle("Tic Tac Toe");
+        setSize(500,500);
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+        currentPlayer = "x";
+        board = new JButton[5][5];
+        hasWinner = false;
+        initializeBoard();
+        initializeMenuBar();
+    }
+     
+    private void initializeMenuBar(){
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+        
+        newGame = new JMenuItem("New Game");
+        newGame.addActionListener(new ActionListener(){
+            
+            @Override
+            public void actionPerformed(ActionEvent e){
+                resetBoard();
+            }
+        });
+        
+        quit = new JMenuItem("Quit");
+        quit.addActionListener(new ActionListener(){
+        
+            @Override
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);
+        }
+    });
+        menu.add(newGame);
+        menu.add(quit);
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+    }
+    private void resetBoard(){
+        currentPlayer = "x";
+        hasWinner = false;
+        for(int i = 0; i <5; i++){
+            for(int j = 0; j < 5; j++){
+                board[i][j].setText("");
+            }
+    }
+    }
+    private void initializeBoard(){
+        for (int i = 0; i<5; i++){
+            for (int j = 0; j<5; j++){
+                JButton btn = new JButton();
+                btn.setFont(new Font(Font.SANS_SERIF,Font.BOLD,100));
+                board[i][j] = btn;
+                btn.addActionListener(new ActionListener(){
+                    
+                    @Override
+                    public void actionPerformed(ActionEvent e){
+                        if(((JButton)e.getSource()).getText().equals("") && hasWinner == false){
+                            btn.setText(currentPlayer);
+                            hasWinner();
+                            togglePlayer();
+                        }
+                    }
+                });
+                pane.add(btn);
+            }
+        }
+    }
+    private void togglePlayer(){
+        if (currentPlayer.equals("x"))
+            currentPlayer = "o";
+        else
+            currentPlayer = "x";
+    }
+
+    private void hasWinner() {
+    // Check rows
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j <= 2; j++) {
+            if (checkCombination(board[i][j], board[i][j+1], board[i][j+2])) {
+                return;
+            }
+        }
+    }
+
+    // Check columns
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j <= 2; j++) {
+            if (checkCombination(board[j][i], board[j+1][i], board[j+2][i])) {
+                return;
+            }
+        }
+    }
+
+    // Check diagonals
+    for (int i = 0; i <= 2; i++) {
+        for (int j = 0; j <= 2; j++){
+            if (checkCombination(board[i][j], board[i+1][j+1], board[i+2][j+2])) {
+                return;
+            }
+        }
+    }
+
+    for (int i = 2; i <= 4; i++) {
+        for (int j = 4; j >= 2; j--){
+            if (checkCombination(board[i][j-2], board[i-1][j-1], board[i-2][j])) {
+                return;
+            }
+        }
+    }
+    }
+    
+    private boolean checkCombination(JButton button1, JButton button2, JButton button3) {
+    String text = button1.getText();
+    if (!text.equals("") && text.equals(button2.getText()) && text.equals(button3.getText())) {
+        JOptionPane.showMessageDialog(null, "Player " + text + " has won");
+        hasWinner = true;
+        return true;
+    }
+    return false;
+}
+}
