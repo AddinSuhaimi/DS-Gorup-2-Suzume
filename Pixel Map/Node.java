45d637bdf5e96b69a4cf89b517b44af573f8d3da
AfifNawawi <116179435+AfifNawawi@users.noreply.github.com>
2023-06-15 16:32:30 +0800

    Create Node.java

---
+++ Pixel Map/Node.java
@@ -0,0 +1,19 @@
+package ds-group-2-suzume;
+
+import java.util.HashSet;
+
+public class Node {
+    int row;
+    int col;
+    int dist;
+    HashSet<String> visited;
+    Node parent;
+
+    Node(int row, int col, int dist, HashSet<String> visited, Node parent) {
+        this.row = row;
+        this.col = col;
+        this.dist = dist;
+        this.visited = visited;
+        this.parent = parent;
+    }
+}
