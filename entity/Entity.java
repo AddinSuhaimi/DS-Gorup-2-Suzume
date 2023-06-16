70c85697b0caf443231dc81bb1acb46b6e0c8fe9
Afnan-Akram <116626398+Afnan-Akram@users.noreply.github.com>
2023-06-17 03:02:58 +0800

    Create Entity.java

---
+++ entity/Entity.java
@@ -0,0 +1,21 @@
+/*
+
+ */
+package entity;
+
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author 123456
+ */
+public class Entity extends Rectangle{
+    public int x, y;
+    public int speed;
+    
+    public BufferedImage img;
+   
+    public Rectangle solidArea;
+    public boolean collisionOn = false;
+}
