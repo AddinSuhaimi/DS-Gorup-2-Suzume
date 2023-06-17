e599bf5b2e2735efd2a3f0b54519baa5f5725909
AddinSuhaimi <132387859+AddinSuhaimi@users.noreply.github.com>
2023-06-17 17:55:25 +0800

    Create MainGame.java

---
+++ FullGame/maingame/MainGame.java
@@ -0,0 +1,27 @@
+package maingame;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author 123456
+ */
+public class MainGame {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Journey-Simulation");
+        
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        
+        window.pack();
+        
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        
+        gamePanel.startGameThread();
+        
+    }
+}
