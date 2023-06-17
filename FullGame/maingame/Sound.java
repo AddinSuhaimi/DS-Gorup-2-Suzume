40ac50e64af599c3fa4b9d5f41db77f9ac408cd9
Afnan-Akram <116626398+Afnan-Akram@users.noreply.github.com>
2023-06-18 01:42:08 +0800

    Update Sound.java

--- FullGame/maingame/Sound.java
+++ FullGame/maingame/Sound.java
@@ -15,46 +15,64 @@ import javax.sound.sampled.Clip;
  * @author 123456
  */
 public class Sound {
-    Clip clip;
-   File soundFile[] = new File[4];
+   File[] soundFiles = new File[4];
+    Clip[] clips = new Clip[4];
 
     public Sound() {
-        soundFile[0] = new File("BGM.wav");
-        soundFile[1] = new File("battle.wav");
-        soundFile[2] = new File("win.wav");
-        soundFile[3] = new File("gameover.wav");
+        soundFiles[0] = new File("BGM.wav");
+        soundFiles[1] = new File("battle.wav");
+        soundFiles[2] = new File("win.wav");
+        soundFiles[3] = new File("gameover.wav");
+
+        try {
+            for (int i = 0; i < soundFiles.length; i++) {
+                AudioInputStream ais = AudioSystem.getAudioInputStream(soundFiles[i]);
+                clips[i] = AudioSystem.getClip();
+                clips[i].open(ais);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public void setFile(int i){
-        try{
-             if (clip != null) {
+       if (i >= 0 && i < clips.length) {
+            Clip clip = clips[i];
+            if (clip != null) {
                 clip.stop();
-                clip.close();
+                clip.setFramePosition(0);
             }
-           AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile[i]);
-            clip = AudioSystem.getClip();
-            clip.open(ais);
-        }
-        catch(Exception e){
-            
         }
     }
     public void play() {
-        if (clip != null) {
-            clip.setFramePosition(0);
-            clip.start();
+        if (clips[0] != null) {
+            clips[0].loop(Clip.LOOP_CONTINUOUSLY);
         }
     }
 
     public void loop() {
-        if (clip != null) {
-            clip.setFramePosition(0);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        for (Clip clip : clips) {
+            if (clip != null) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
         }
     }
 
     public void stop() {
-        if (clip != null && clip.isRunning()) {
-            clip.stop();
+        for (Clip clip : clips) {
+            if (clip != null) {
+                clip.stop();
+            }
+        }
+    }
+
+    public void playSE(int i) {
+        if (i > 0 && i < clips.length && clips[i] != null) {
+            Clip clip = clips[i];
+            if (clip.isRunning()) {
+                clip.stop();
+            }
+            clip.setFramePosition(0);
+            clip.start();
         }
     }
 }
